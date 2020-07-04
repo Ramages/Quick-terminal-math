df5876ed297ee14d1e327df90d952905aece177f
s9rA16Bf4 <nicholas.rosqvist.nunes@gmail.com>
2020-07-04 22:21:43 +0200

    Update math.cpp

--- src/math.cpp
+++ src/math.cpp
@@ -10,6 +10,6 @@ std::string mult(std::string a, std::string b){ return std::to_string(std::stof(
 
 std::string mod(std::string a, std::string b){ return std::to_string(std::stof(a) % std::stof(b)); }
 
-std::string rise(std::string a, std::string b){ return std::to_string(pow(std::stof(a) / std::stof(b))); }
+std::string rise(std::string a, std::string b){ return std::to_string(pow(std::stof(a) ,std::stof(b))); }
 
-std::string sqrtL(std::string a){ return std::to_string(sqrt(std::stof(a) / std::stof(b))); }
+std::string sqrtL(std::string a){ return std::to_string(sqrt(std::stof(a))); }
