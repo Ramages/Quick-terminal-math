4871f2fd244d875a062fe0b4fe891f6f93c6efe0
s9rA16Bf4 <nicholas.rosqvist.nunes@gmail.com>
2020-07-04 22:44:32 +0200

    Update math.cpp
    Added ln and lg

--- src/math.cpp
+++ src/math.cpp
@@ -13,3 +13,7 @@ std::string mod(std::string a, std::string b){ return std::to_string(std::stoi(a
 std::string rise(std::string a, std::string b){ return std::to_string(pow(std::stof(a) ,std::stof(b))); }
 
 std::string sqrtL(std::string a){ return std::to_string(sqrt(std::stof(a))); }
+
+std::string ln(std::string a){ return std::to_string(log(std::stof(a))); }
+
+std::string lg(std::string a){ return std::to_string(log10(std::stof(a))); }
