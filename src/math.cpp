09e9013c6bd204deb0e2cc477973c0c1078230cf
Ramages <51058775+Ramages@users.noreply.github.com>
2021-12-13 14:34:12 +0100

    Add files via upload

--- src/math.cpp
+++ src/math.cpp
@@ -1,12 +1,71 @@
 #include "math.hpp"
 
-std::string add(std::string a, std::string b){ return std::to_string(std::stof(a) + std::stof(b)); }
+std::string add(std::string a, std::string b)
+{
+	std::string returnStr;
 
-std::string sub(std::string a, std::string b){ return std::to_string(std::stof(a) - std::stof(b)); }
+	float retFloat = std::stof(a) + std::stof(b);
+	if(floor(retFloat) == retFloat)
+	{
+		int helper = (int)retFloat;
+		returnStr = std::to_string(helper);
+	}
+	else
+	{
+		returnStr = std::to_string(retFloat);
+	}
+	return returnStr;
+}
 
-std::string divL(std::string a, std::string b){ return std::to_string(std::stof(a) / std::stof(b)); }
+std::string sub(std::string a, std::string b)
+{
+	std::string returnStr;
 
-std::string mult(std::string a, std::string b){ return std::to_string(std::stof(a) * std::stof(b)); }
+	float retFloat = std::stof(a) - std::stof(b);
+	if(floor(retFloat) == retFloat)
+	{
+		int helper = (int)retFloat;
+		returnStr = std::to_string(helper);
+	}
+	else
+	{
+		returnStr = std::to_string(retFloat);
+	}
+	return returnStr;
+}
+
+std::string divL(std::string a, std::string b)
+{
+	std::string returnStr;
+
+	float retFloat = std::stof(a) / std::stof(b);
+	if(floor(retFloat) == retFloat)
+	{
+		int helper = (int)retFloat;
+		returnStr = std::to_string(helper);
+	}
+	else
+	{
+		returnStr = std::to_string(retFloat);
+	}
+	return returnStr;
+}
+std::string mult(std::string a, std::string b)
+{
+	std::string returnStr;
+
+	float retFloat = std::stof(a) * std::stof(b);
+	if(floor(retFloat) == retFloat)
+	{
+		int helper = (int)retFloat;
+		returnStr = std::to_string(helper);
+	}
+	else
+	{
+		returnStr = std::to_string(retFloat);
+	}
+	return returnStr;
+}
 
 std::string mod(std::string a, std::string b){ return std::to_string(std::stoi(a) % std::stoi(b)); }
 
