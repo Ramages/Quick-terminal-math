22edae5843d38fb7a7ffc9429835fdd5bccadcfa
Ramages <51058775+Ramages@users.noreply.github.com>
2021-12-13 15:01:18 +0100

    Add files via upload
    Updated the other operations to also round up whole numbers and cast them to ints (rather than floats) before returning the result

--- src/math.cpp
+++ src/math.cpp
@@ -69,10 +69,70 @@ std::string mult(std::string a, std::string b)
 
 std::string mod(std::string a, std::string b){ return std::to_string(std::stoi(a) % std::stoi(b)); }
 
-std::string rise(std::string a, std::string b){ return std::to_string(pow(std::stof(a) ,std::stof(b))); }
+std::string rise(std::string a, std::string b)
+{
+	std::string returnStr;
 
-std::string sqrtL(std::string a){ return std::to_string(sqrt(std::stof(a))); }
+	float retFloat = pow(std::stof(a), std::stof(b));
+	if(floor(retFloat) == retFloat)
+	{
+		int helper = (int)retFloat;
+		returnStr = std::to_string(helper);
+	}
+	else
+	{
+		returnStr = std::to_string(retFloat);
+	}
+	return returnStr;
+}
 
-std::string ln(std::string a){ return std::to_string(log(std::stof(a))); }
+std::string sqrtL(std::string a)
+{
+	std::string returnStr;
 
-std::string lg(std::string a){ return std::to_string(log10(std::stof(a))); }
+	float retFloat = sqrt(std::stof(a));
+	if(floor(retFloat) == retFloat)
+	{
+		int helper = (int)retFloat;
+		returnStr = std::to_string(helper);
+	}
+	else
+	{
+		returnStr = std::to_string(retFloat);
+	}
+	return returnStr;
+}
+
+std::string ln(std::string a)
+{
+	std::string returnStr;
+
+	float retFloat = log(std::stof(a));
+	if(floor(retFloat) == retFloat)
+	{
+		int helper = (int)retFloat;
+		returnStr = std::to_string(helper);
+	}
+	else
+	{
+		returnStr = std::to_string(retFloat);
+	}
+	return returnStr;
+}
+
+std::string lg(std::string a)
+{
+	std::string returnStr;
+
+	float retFloat = log10(std::stof(a));
+	if(floor(retFloat) == retFloat)
+	{
+		int helper = (int)retFloat;
+		returnStr = std::to_string(helper);
+	}
+	else
+	{
+		returnStr = std::to_string(retFloat);
+	}
+	return returnStr;
+}
